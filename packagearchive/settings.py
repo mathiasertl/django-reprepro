98831f00d273aa7239f6353c2e50a75a8437d328
Mathias Ertl <mati@er.tl>
2015-12-03 13:26:05 +0100

    move from South to standard django migrations

--- packagearchive/settings.py
+++ packagearchive/settings.py
@@ -120,8 +120,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'south',
-
     'distribution',
     'incoming',
     'package',
