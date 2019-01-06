20ec45f60a721688b28d5ff7498f86ebb6854613
Mathias Ertl <mati@er.tl>
2019-01-06 14:01:50 +0100

    add code_quality command

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+import subprocess
+import sys
+
+from setuptools import setup
+from setuptools import Command
 
 install_requires = [
     'Django>=2.1',
@@ -8,9 +12,37 @@ install_requires = [
     'six>=1.11'
 ]
 
+
+class QualityCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        checked = ['packagearchive', 'django_reprepro', 'fabfile.py', 'setup.py', ]
+        print('isort --check-only --diff -rc %s' % ' '.join(checked))
+        status = subprocess.call(['isort', '--check-only', '--diff', '-rc', ] + checked)
+        if status != 0:
+            sys.exit(status)
+
+        print('flake8 %s' % ' '.join(checked))
+        status = subprocess.call(['flake8', ] + checked)
+        if status != 0:
+            sys.exit(status)
+
+        print('python -Wd manage.py check')
+        status = subprocess.call(['python', '-Wd', 'manage.py', 'check'])
+        if status != 0:
+            sys.exit(status)
+
+
 setup(
     name='django-reprepro',
-    version='0.1.0b1',
+    version='0.2.0',
     description='',
     author='Mathias Ertl',
     author_email='mati@er.tl',
@@ -21,6 +53,9 @@ setup(
         'django_reprepro.management.commands',
         'django_reprepro.migrations',
     ],
+    cmdclass={
+        'code_quality': QualityCommand,
+    },
     install_requires=install_requires,
     classifiers=[
         'Framework :: Django',
